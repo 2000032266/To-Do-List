7230f2c1bf0a4ced8ce642aa7763563fd25a43e7
2000032266 <2000032266@kluniversity.in>
2024-11-27 16:43:13 +0530

    Updates

---
+++ precache-manifest.6fbcf2d7f943a36679e9be227baf064d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "142e9e3f5cd64b410a9fed57f61232ba",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "cf9ecf11c9246d4ce939",
+    "url": "/To-Do-List/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "c3893bf00d87859c6c2d",
+    "url": "/To-Do-List/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cf9ecf11c9246d4ce939",
+    "url": "/To-Do-List/static/js/2.e2dc2d9e.chunk.js"
+  },
+  {
+    "revision": "65a9926ecde5afbe3bda",
+    "url": "/To-Do-List/static/js/3.3b6fb9ae.chunk.js"
+  },
+  {
+    "revision": "c3893bf00d87859c6c2d",
+    "url": "/To-Do-List/static/js/main.242819fe.chunk.js"
+  },
+  {
+    "revision": "43701b423c73ece00f7a",
+    "url": "/To-Do-List/static/js/runtime~main.7d48cc21.js"
+  }
+]);
